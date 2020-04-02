3171493ddeb83b6ba211d7e84ff62d2c29950ee0
mariagnoriegag <maria.noriega@utec.edu.pe>
2020-04-02 09:44:53 -0500

    fonts

---
+++ general/Fonts.ts
@@ -0,0 +1,19 @@
+import FontFaceObserver from "fontfaceobserver";
+
+const minskyGoogleFonts = "https://fonts.googleapis.com/css?family=Karla:400,700|Rubik:400,700";
+
+const Fonts = () => {
+	const link = document.createElement("link");
+	link.href = minskyGoogleFonts;
+	link.rel = "stylesheet";
+
+	document.head.appendChild(link);
+
+	const karla = new FontFaceObserver("Karla");
+
+	karla.load().then(() => {
+		document.documentElement.classList.add("minsky");
+	});
+};
+
+export default Fonts;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,50 @@
 import { NextPage } from "next";
 import { styled } from "linaria/react";
+import { css } from "linaria";
 // import { motion } from "framer-motion";
 import CollectaLogo from "../components/atoms/CollectaLogo";
 import CollectaIsotype from "../components/atoms/CollectaIsotype";
 
+export const globals = css`
+	:global() {
+		html {
+			box-sizing: border-box;
+		}
+		body {
+			margin: 0px;
+		}
+
+		*,
+		*:before,
+		*:after {
+			box-sizing: inherit;
+		}
+	}
+`;
+
 const Center = styled.div`
-    width: 100%;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
+	width: 100%;
+	height: 100vh;
+	max-height: 100vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	text-align: center;
 `;
 
 const Home: NextPage<{ userAgent: string }> = ({ userAgent }) => {
-    return (
-        <Center>
-            <div>
-                <CollectaLogo scale={0.6} />
-            </div>
-        </Center>
-    );
+	return (
+		<Center>
+			<div>
+				<CollectaLogo scale={0.6} />
+			</div>
+		</Center>
+	);
 };
 
 Home.getInitialProps = async ({ req }) => {
-    const userAgent = req ? req.headers["user-agent"] || "" : navigator.userAgent;
-    return { userAgent };
+	const userAgent = req ? req.headers["user-agent"] || "" : navigator.userAgent;
+	return { userAgent };
 };
 
 export default Home;
